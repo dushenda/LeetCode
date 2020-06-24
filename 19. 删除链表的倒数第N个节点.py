348685ed45d7a43e11f3afa7529238a525f4db1a
dushenda <dushenda@outlook.com>
2020-06-24 17:07:36 +0800

    624-19

---
+++ 19. 删除链表的倒数第N个节点.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+@ide        : PyCharm
+@project    : LeeCode
+@file       : 19. 删除链表的倒数第N个节点.py
+@author     : CALIBRATION
+@time       : 2020/6/24 16:42
+@description: None
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        i = head
+        list_Node = []
+        while i.next:
+            list_Node.append(i)
+            i = i.next
+        list_Node.append(i)
+        lens = len(list_Node)
+        if lens == 1:
+            list_Node[0] = None
+        elif lens == 2:
+            if n == 1:
+                list_Node[0].next = None
+                list_Node[1] = None
+                list_Node.pop(1)
+            else:
+                list_Node[0] = list_Node[1]
+                list_Node[1] = None
+                list_Node.pop(1)
+        else:
+            if n == 1:
+                list_Node[lens - n - 1].next = None
+            else:
+                list_Node[lens - n - 1].next = list_Node[lens - n + 1]
+                list_Node[lens - n] = None
+                list_Node.pop(lens - n)
+        return list_Node[0]
+
+
+def main():
+    ln1 = ListNode(1)
+    ln2 = ListNode(2)
+    ln3 = ListNode(3)
+    ln1.next = ln2
+    ln2.next = ln3
+    s = Solution()
+    res = s.removeNthFromEnd(ln1, 1)
+    while res.next:
+        print(res.val)
+        res = res.next
+    print(res.val)
+
+
+if __name__ == '__main__':
+    main()
