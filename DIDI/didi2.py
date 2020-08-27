6007ab0689d9e77005f23bc276480029fef4211e
dushenda <dushenda@outlook.com>
2020-08-27 15:47:43 +0800

    827

---
+++ DIDI/didi2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+@ide        : PyCharm
+@project    : LeetCode
+@file       : didi2.py
+@author     : CALIBRATION
+@time       : 2020/8/21 20:01
+@description: None
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
